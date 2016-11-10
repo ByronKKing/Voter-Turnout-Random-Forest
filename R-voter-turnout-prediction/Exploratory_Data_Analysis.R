c15e9cd091661ca72843f293bbc1cf879475d737
Byron King <byronkking@gmail.com>
2016-11-09 22:35:40 -0600

    add comments and white space

--- R-voter-turnout-prediction/Exploratory_Data_Analysis.R
+++ R-voter-turnout-prediction/Exploratory_Data_Analysis.R
@@ -1,8 +1,12 @@
-#read file into 'original'
-setwd('/Users/peterking/Desktop/0ptimus Project')
+# Read file into 'original'
+
+setwd('/Users/bkking/Desktop/0ptimus Project')
 original = read.csv("voterfile.csv")
 str(original)
-#create temp from original, make voter history factor variables
+
+
+
+# Create temp from original, make voter history factor variables
 temp<-original
 voterhist<-temp[12:26]
 temp<-temp[,-(12:26),drop=FALSE]
@@ -11,102 +15,129 @@ voterhist[] <- lapply(voterhist, factor,
                labels = c("No", "Yes"))
 temp<-data.frame(temp,voterhist)
 
-###########missing data analysis using VIM package
+
+
+# Missing data analysis using VIM package
+
 library(VIM)
-#convert nan and 'unknown' to missing
+
+##convert nan and 'unknown' to missing
 is.na(temp) <- temp=="nan"
 is.na(temp) <- temp=="Unknown"
-#call missing data frequency per variable and plot missing data patterns
+
+##call missing data frequency per variable and plot missing data patterns
 missing<-aggr(temp,bars=FALSE,combined=TRUE,sortVars=FALSE)
 missing
-#drop voter histories (no missing values) and turnout probs (1 missing value each)
+
+##drop voter histories (no missing values) and turnout probs (1 missing value each)
 simple<-temp[,-(25:39),drop=FALSE]
 simpler<-simple[,-(19:24),drop=FALSE]
-###call missing data frequency again
+
+##call missing data frequency again
 missing<-aggr(simpler,bars=FALSE,combined=TRUE,sortVars=FALSE)
 missing
-#delete all data impossible to impute (pets,nascar,instruments)
+
+##delete all data impossible to impute (pets,nascar,instruments)
 simpler<-simpler[,-(13:15),drop=FALSE]
-#exclude variables with missing values above 40,000 (donates to liberal/conservative,occupationindustry)
+
+##exclude variables with missing values above 40,000 (donates to liberal/conservative,occupationindustry)
 simplest<-simpler[colSums(is.na(simpler)) < 40000]
-###plot missing data freq again
-#seems to be little trend in missing values--evidence for MCAR
+
+##plot missing data freq again
+###seems to be little trend in missing values--evidence for MCAR
 missing<-aggr(simplest,bars=FALSE,combined=TRUE,sortVars=FALSE)
 missing
-###dataset with variables with missing values above 20,000
-#seems to be little trend in vars with most missing values--evidence for MCAR
+
+##create temp dataframe with variables with missing values above 20,000
+###seems to be little trend in vars with most missing values--evidence for MCAR
 mostmissing<-simplest[colSums(is.na(simplest)) > 20000]
 mostmiss<-aggr(mostmissing,bars=TRUE,numbers=FALSE,sortVars=TRUE,cex.axis=.7, gap=.7)
 mostmiss
-###dataset with variables with missing values below 20,000
-#still seems to be little trend in vars with least missing values--evidence for MCAR
+
+##create temp dataframe with variables with missing values below 20,000
+###still seems to be little trend in vars with least missing values--evidence for MCAR
 leastmissing<-simplest[colSums(is.na(simplest)) < 20000]
 leastmiss<-aggr(leastmissing,bars=TRUE,numbers=FALSE,sortVars=TRUE,cex.axis=.7, gap=.7)
 leastmiss
-###any correlation with other particular vars?
-#age and ethnicity--MCAR--no significant trend
+
+##examine correlation with other variables
+
+###age and ethnicity--MCAR--no significant trend
 x <- leastmissing[, c("age", "ethnicity")]
 barMiss(x)
 barMiss(x, only.miss = FALSE)
-#age and income--MAR--voters in early 20s more likely to have missing income values...
+
+###age and income--MAR--voters in early 20s more likely to have missing income values...
 y <- leastmissing[, c("age", "income")]
 barMiss(y)
 barMiss(y, only.miss = FALSE)
-#ethnicity and income--MCAR--no significant missing data trend
+
+###ethnicity and income--MCAR--no significant missing data trend
 z <- leastmissing[, c("ethnicity", "income")]
 barMiss(z)
 barMiss(z, only.miss = FALSE)
 
-###########descriptive stats, general exploratory data analysis
-#####party
+
+
+# Descriptive stats and eneral exploratory data analysis
+
+## Examine party
 summary(original$party)
 df<-data.frame(table(original$party))
 colnames(df)<-c('Party','Freq')
-#pie chart with all parties
+
+###create pie chart with all parties
 barplot<- ggplot(data=df, aes(x="", y=Freq, fill=Party))+geom_bar(width = 1, stat = "identity")
 pie_parties <- barplot + coord_polar("y", start=0) +
   labs(title="Party Frequency") +
   theme(axis.text.x=element_blank())
-#code parties other than dem and repub as 'other'
+
+###code parties other than dem and repub as 'other'
 temp$party<-recode(temp$party, "'Democratic'='Democrat';'Republican'='Republican';
                     'American Independent'='Other';'Green'='Other';'Libertarian'='Other';
                     'Natural Law'='Other';'Non-Partisan'='Other';'Other'='Other'")
 df1<-data.frame(table(temp$party))
 colnames(df1)<-c('Party','Frequency')
-#barchart with abridged party distinctions
+
+###create barchart with abridged party distinctions
 ggplot(df1, aes(x=Party, y=Frequency, fill=Party))+
   geom_bar(width = 1, stat = "identity")+
   scale_fill_manual(values=c("blue","pink","red"))+
   labs(title="Party Frequency",x = "Party",y="Frequency")
-#####ethnicity
+
+## Ethnicity
 summary(original$ethnicity)
 df2<-data.frame(table(original$ethnicity))
 colnames(df2)<-c('Ethnicity','Frequency')
 ggplot(data=df2, aes(x=Ethnicity, y=Frequency, fill=Ethnicity)) +
   geom_bar(stat="identity", position=position_dodge())+
   labs(title="Ethnicity Frequency",x = "Ethnicity",y="Frequency")
-#####age
+
+## Age
 summary(original$age)
 ggplot(data=original, aes(original$age)) + 
   geom_histogram(binwidth=5, colour="black",fill="white")+
   labs(title="Age Distribution",x = "Age",y="Count")
-#####age vs ethnicity
+
+
+## Age and ethnicity
 ggplot(data=original, aes(x=original$ethnicity, y=original$age,
-                                   ,fill=original$ethnicity)) + 
+            fill=original$ethnicity)) + 
             geom_boxplot()+
             labs(title="Ethnicity by Age",
              x = "Ethnicity",y="Age",fill="Ethnicity")
-#####age vs party
+## Age vs party
 ggplot(data=original, aes(x=original$party, y=original$age,
-                                   ,fill=original$party)) + geom_boxplot() +
+              fill=original$party)) + geom_boxplot() +
               labs(title="Party by Age", x = "Party",y="Age",fill="Party")
-#w/ 'other' coding
+### With 'other' coding
 attach(temp)
 ggplot(data=temp, aes(x=party, y=age,
-                                ,fill=party)) + geom_boxplot()+ 
-  labs(title="Party by Age",x = "Party",y="Age",fill="Party")
+              fill=party)) + geom_boxplot()+ 
+              labs(title="Party by Age",x = "Party",y="Age",fill="Party")
 detach(temp)
-#####plot ethnicity vs. party
+
+## Plot ethnicity vs. party
 attach(temp)
 contingency<-table(ethnicity,party)
 ethnicity_party<-data.frame(contingency)
@@ -115,47 +146,61 @@ newbarchart<-ggplot(data=ethnicity_party, aes(x=ethnicity, y=Freq, fill=party)) 
 newbarchart + labs(title="Party by Ethnicity",
                 x = "Ethnicity",y="Frequency",fill="Party") +
                 scale_fill_manual(values=c("blue","pink","red"))
-#####general/primary turnout rates
-#create data frame with turnout numbers
+
+
+
+# Analyze general/primary turnout rates
+
+##create data frame with turnout numbers
 turnout_years<-as.data.frame(summary(temp[,25:39]))
-#create function to extract total turnout number for each election type
+
+##create function to extract total turnout number for each election type
 library(gdata)
 turnout_years$Freq <- as.character(turnout_years$Freq)
 turnout_years$Freq[1]
 trim(strsplit(turnout_years$Freq[1], split='[:]'))
 trim(strsplit(turnout_years$Freq[1], split='[:]')[[1]][2])
 turnout_years$turnout<-sapply(turnout_years$Freq, function(x) {trim(strsplit(x, split='[:]')[[1]][2])})
-#delete unnecessary variables
+
+##delete unnecessary variables
 turnout_years<-turnout_years[,-c(1,3)]
-#select only number that turned out
+
+##select only number that turned out
 turnout<- turnout_years[seq(2, nrow(turnout_years), by=2), ]
-#extract general or primary
+
+##extract general or primary
 turnout$Var2<-as.character(turnout$Var2)
 turnout$Var2[1]
 substr(turnout$Var2[1],5,5)
 turnout$election<-sapply(turnout$Var2, function(x) {substr(x, 5, 5)})
 turnout$election<- ifelse(turnout$election == 'p', 'primary', 'general')
-#extract year
+
+##extract year
 turnout$year<-sapply(turnout$Var2, function(x) {substr(x, 3, 4)})
-#convert chars to nums, delete missing variables
+
+##convert chars to nums, delete missing variables
 turnout$year<-as.numeric(turnout$year)
 turnout$turnout<-as.numeric(turnout$turnout)
 turnout<-turnout[,-1]
 turnout$year<-turnout$year+2000
-###plot linegraph
+
+
+##plot linegraph
 ggplot(data=turnout, aes(x=year, y=turnout, group=election, colour=election)) +
   geom_line() +
   geom_point()+
   labs(title="Turnout by Election",
        x = "Election Year",y="Turnout",fill="Electon Type")+
   geom_abline()
-###plot bargraph
+
+##plot bargraph
 turnout_barchart<-ggplot(data=turnout, aes(x=year, y=turnout, fill=election)) +
   geom_bar(stat="identity", position=position_dodge())
 turnout_barchart + labs(title="Turnout by Election",
                         x = "Election Year",y="Turnout",fill="Electon Type") +
   scale_fill_manual(values=c("blue","turquoise"))
-###general election turnout
+
+##general election turnout
 turnout_general<-turnout[ !c(TRUE,FALSE), ]
 turnout_general
 ggplot(data=turnout_general, aes(x=year, y=turnout, colour=election)) +
--- R-voter-turnout-prediction/Second_Preliminary_Analysis.R
+++ R-voter-turnout-prediction/Second_Preliminary_Analysis.R
@@ -1,5 +1,6 @@
-#################prepare second dataset for analysis
-#make voter history factor variables, nan/unknown are missing
+# Prepare second dataset for analysis
+
+##make voter history factor variables, nan/unknown are missing
 setwd('/Users/peterking/Desktop/0ptimus Project')
 original = read.csv("voterfile.csv")
 temp<-original
@@ -11,15 +12,19 @@ voterhist[] <- lapply(voterhist, factor,
 temp<-data.frame(temp,voterhist)
 is.na(temp) <- temp=="nan"
 is.na(temp) <- temp=="Unknown"
-#delete pets, instruments, nascar
+
+##delete pets, instruments, nascar
 analysis1<-temp[ , -which(names(temp) %in% c("intrst_nascar_in_hh","intrst_musical_instruments_in_hh","petowner_dog",
                                              "donates_to_liberal_causes","donates_to_conservative_causes","occupationindustry"))]
-#modify dataset--new dataset w/ vars. below 23,000 missing values (i.e. include education)
+
+##modify dataset--new dataset w/ vars. below 23,000 missing values (i.e. include education)
 analysis1<-analysis1[colSums(is.na(analysis1)) < 23000]
-#dataset w/ complete cases of these variables for random forest
-#contains 7 vars that have less than 20,000 missing vars.
+
+##dataset w/ complete cases of these variables for random forest
+###contains 7 vars that have less than 20,000 missing vars.
 analysis1<-analysis1[complete.cases(analysis1),]
-#################code response variable
+
+##code response variable
 analysis1$gen12= ifelse (analysis1$vh12g=="Yes", 7, 0)
 analysis1$gen10= ifelse (analysis1$vh10g=="Yes", 6, 0)
 analysis1$gen08= ifelse (analysis1$vh08g=="Yes", 5, 0)
@@ -28,37 +33,46 @@ analysis1$gen04= ifelse (analysis1$vh04g=="Yes", 3, 0)
 analysis1$gen02= ifelse (analysis1$vh02g=="Yes", 2, 0)
 analysis1$gen00= ifelse (analysis1$vh00g=="Yes", 1, 0)
 analysis1$genhist<-rowSums( analysis1[,30:36] )
-#find median, if above median then 1, if not 0
+
+##find median, if above median then 1, if not 0
 summary(analysis1$genhist)
 analysis1$turnout= ifelse (analysis1$genhist>10, 1, 0)
 analysis1$turnout=as.factor(analysis1$turnout)
 summary(analysis1$turnout)
-#delete gen voter history variables
+
+##delete gen voter history variables
 analysis1<-analysis1[ , -which(names(analysis1) %in% c("vh12g","vh10g","vh08g",
                                                        "vh06g","vh04g","vh02g",
                                                        "vh00g","gen12","gen10","gen08",
                                                        "gen06","gen04","gen02","gen00","genhist"))]
 
-#################random forest algorithm
-#set seed for replicable results
+
+
+# Random Forest
+
+##set seed for replicable results
 set.seed(8888)
-#create train and test data
+
+##create train and test data
 library(caret)
 trainindex <- createDataPartition(analysis1$turnout, p = .8,
                                   list = FALSE,
                                   times = 1)
 analysis1Train <- analysis1[ trainindex,]
 analysis1Test  <- analysis1[-trainindex,]
-#run random forest algorithm
+
+##run random forest algorithm
 library(randomForest)
 RF2 <- randomForest(as.factor(turnout) ~ age + party + ethnicity + income + cd + dma + education +
                       g08_precinct_turnout + g10_precinct_turnout + g12_precinct_turnout +
                       p08_precinct_turnout + p10_precinct_turnout + p12_precinct_turnout +
                       vh14p + vh12p + vh10p + vh08p + vh06p + vh04p + vh02p + vh00p,
                     data=analysis1Train, importance=TRUE, ntree=100)
-#plot important variables
+
+##plot important variables
 varImpPlot(RF2)
-#predict on test data
+
+##predict on test data
 Prediction <- predict(RF2, analysis1Test)
 RFPRED2 <- data.frame(optimus_id = analysis1Test$optimus_id, 
                      turnout_old= analysis1Test$turnout,
